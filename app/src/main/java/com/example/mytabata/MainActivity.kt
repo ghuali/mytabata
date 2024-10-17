6b8124b800dd9564ddc3f07ae74e5f3ffd910754
ghuali <guaghuali@gmail.com>
2024-10-17 12:00:19 +0100

    Se cambia las cosas en clase comenzamos con reset de timer

--- app/src/main/java/com/example/mytabata/MainActivity.kt
+++ app/src/main/java/com/example/mytabata/MainActivity.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
@@ -15,8 +18,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import org.example.mytabata.CounterDown
 import com.example.mytabata.ui.theme.MytabataTheme
 
@@ -41,31 +46,35 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Counter(modifier: Modifier = Modifier) {
     var theCounter by remember { mutableStateOf(0L) }
-    val miCounterDown = CounterDown(99, {newValue -> theCounter = newValue })
+    var miConterDown by remember{ mutableStateOf(CounterDown(99, {newvalue -> theCounter = newvalue}))}
+    var reseteo by remember { mutableStateOf(CounterDown(0,{newvalue -> theCounter = newvalue})) }
 
+    Column(modifier = Modifier.fillMaxSize()
+        .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
 
-
-    Column {
-        Text(
-            text = theCounter.toString(),
-            modifier = modifier
-        )
-        Button(onClick = {
-
-            if (!miCounterDown.counterState) {
-                miCounterDown.myCounter.start()
-                miCounterDown.counterState = true
-            } else {
-                miCounterDown.myCounter.cancel()
-                miCounterDown.counterState = false
-            }
-        }) {
             Text(
-                text = "Pulsar"
+                text = theCounter.toString(),
+                modifier = modifier
             )
+            Button(onClick = {
+                miConterDown.toggle()
+            }) {
+                Text(
+                    text = "Pulsar"
+            )
+            Button(onClick = {
+                reseteo.toggle()
+            }) {
+                Text(
+                    text = "reset"
+                )
+            }
         }
     }
 }
+
 
 
 @Preview(showBackground = true)
--- app/src/main/java/com/example/mytabata/ui/CounterDown.kt
+++ app/src/main/java/com/example/mytabata/ui/CounterDown.kt
@@ -2,22 +2,41 @@ package org.example.mytabata
 
 import android.os.CountDownTimer
 import android.util.Log
+import androidx.compose.runtime.Composable
 
-class CounterDown(var segundos: Int,var loQueHaceAlHacertick: (Long) -> Unit){
-    var myCounter: Any
-    var counterState : Boolean = false
+class CounterDown(var segundos: Int, var loquehacealhacertick: (Long) -> Unit) {
+    private var counterState : Boolean = false
 
-    init{
-         myCounter = object : CountDownTimer(((segundos + 1000L).toLong()), 1000) {
+    val myCounter = object : CountDownTimer((segundos * 1000L), 1000) {
 
-            override fun onTick(millisUntilFinished: Long) {
-                loQueHaceAlHacertick(millisUntilFinished / 1000L)
-
-            }
-
-            override fun onFinish() {
-                counterState = false
-            }
+        override fun onTick(millisUntilFinished: Long) {
+            if (counterState) loquehacealhacertick(millisUntilFinished / 1000)
         }
+
+        override fun onFinish() {
+            counterState = false
+            Log.i("dam2", "mensajito")
+        }
+    }
+
+    fun toggle() {
+        Log.i("dam2", "toggle: $counterState")
+        if (this.counterState){
+            this.cancel()
+
+        } else {
+            Log.i("dam2", "toggle: start")
+            this.start()
+        }
+    }
+
+    fun start() {
+        counterState = true
+        this.myCounter.start()
+    }
+
+    fun cancel() {
+        counterState = false
+        this.myCounter.cancel()
     }
 }
